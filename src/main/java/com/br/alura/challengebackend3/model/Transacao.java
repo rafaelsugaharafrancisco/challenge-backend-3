91ca9ab8531f700a3fd9f229c993cde42e90a5f1
Rafael <rafa.s.francisco@gmail.com>
2022-04-19 00:46:46 -0300

    retirando os getter e setter do lombok na classe modelo

--- src/main/java/com/br/alura/challengebackend3/model/Transacao.java
+++ src/main/java/com/br/alura/challengebackend3/model/Transacao.java
@@ -8,11 +8,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 @Entity(name = "transacoes")
 public class Transacao {
 
@@ -36,4 +31,72 @@ public class Transacao {
 	
 	@Column(unique = true)
 	private LocalDateTime dataHoraTransacao;
+
+	public String getBancoOrigem() {
+		return bancoOrigem;
+	}
+
+	public void setBancoOrigem(String bancoOrigem) {
+		this.bancoOrigem = bancoOrigem;
+	}
+
+	public String getAgenciaOrigem() {
+		return agenciaOrigem;
+	}
+
+	public void setAgenciaOrigem(String agenciaOrigem) {
+		this.agenciaOrigem = agenciaOrigem;
+	}
+
+	public String getContaOrigem() {
+		return contaOrigem;
+	}
+
+	public void setContaOrigem(String contaOrigem) {
+		this.contaOrigem = contaOrigem;
+	}
+
+	public String getBancoDestino() {
+		return bancoDestino;
+	}
+
+	public void setBancoDestino(String bancoDestino) {
+		this.bancoDestino = bancoDestino;
+	}
+
+	public String getAgenciaDestino() {
+		return agenciaDestino;
+	}
+
+	public void setAgenciaDestino(String agenciaDestino) {
+		this.agenciaDestino = agenciaDestino;
+	}
+
+	public String getContaDestino() {
+		return contaDestino;
+	}
+
+	public void setContaDestino(String contaDestino) {
+		this.contaDestino = contaDestino;
+	}
+
+	public double getValorTransacao() {
+		return valorTransacao;
+	}
+
+	public void setValorTransacao(double valorTransacao) {
+		this.valorTransacao = valorTransacao;
+	}
+
+	public LocalDateTime getDataHoraTransacao() {
+		return dataHoraTransacao;
+	}
+
+	public void setDataHoraTransacao(LocalDateTime dataHoraTransacao) {
+		this.dataHoraTransacao = dataHoraTransacao;
+	}
+
+	public Long getId() {
+		return id;
+	}
 }
